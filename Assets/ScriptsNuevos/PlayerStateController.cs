243a444260981066444bbc8ff94c4d1c6d391176
AnjalesUPB <88017478+AnjalesUPB@users.noreply.github.com>
2021-11-04 10:43:29 -0500

    PlayerState Script (#6)
    * PlayerState
    
    Script creado
    
    * Script changes
    
    * Changes 2
    
    * Final Script

---
+++ Assets/ScriptsNuevos/PlayerStateController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateController : MonoBehaviour
+{
+    PlayerMovement move;
+    [SerializeField]float lostSpeed = 0;
+    [SerializeField] private casesSO channel;
+    float temp;
+
+    void Start()
+    {
+        move = GetComponent<PlayerMovement>();
+        temp = move.speed;
+    }
+
+    private void OnEnable()
+    {
+        channel.OnJulianHit += StateMachine;
+    }
+
+    private void OnDisable()
+    {
+        channel.OnJulianHit -= StateMachine;
+    }
+
+    void StateMachine(int type)
+    {
+        switch (type)
+        {
+            case 1:
+                Slow();
+                break;
+            case 2:
+                Root();
+                break;
+            case 3:
+                Drunk();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void Slow()
+    {
+        move.speed = lostSpeed;
+    }
+
+    void Root()
+    {
+        move.speed = 0;
+    }
+
+    void Drunk()
+    {
+        move.speed = -temp;
+    }
+}
